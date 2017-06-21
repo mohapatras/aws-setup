e7c7d252efc20fde13507b07424b262d524b3638
mohapatras <bhabanimohapatra@hotmail.com>
2017-06-21 14:25:09 +0530

    added libcurl4 to script

--- install-gpu.sh
+++ install-gpu.sh
@@ -5,6 +5,7 @@ sudo apt-get update
 sudo apt-get --assume-yes upgrade
 sudo apt-get --assume-yes install tmux build-essential gcc g++ make binutils
 sudo apt-get --assume-yes install software-properties-common
+sudo apt install libcurl4-openssl-dev 
 
 # download and install GPU drivers
 wget "http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.44-1_amd64.deb" -O "cuda-repo-ubuntu1604_8.0.44-1_amd64.deb"
@@ -58,9 +59,3 @@ jupass=`python -c "from notebook.auth import passwd; print(passwd())"`
 echo "c.NotebookApp.password = u'"$jupass"'" >> $HOME/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.ip = '*'
 c.NotebookApp.open_browser = False" >> $HOME/.jupyter/jupyter_notebook_config.py
-
-# clone the fast.ai course repo and prompt to start notebook
-cd ~
-git clone https://github.com/fastai/courses.git
-echo "\"jupyter notebook\" will start Jupyter on port 8888"
-echo "If you get an error instead, try restarting your session so your $PATH is updated"
